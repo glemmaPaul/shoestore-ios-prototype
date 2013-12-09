1f07813e78c52259721373a5650f48e55eaf608a
ttg-fueled <ttg@fueled.com>
2013-12-09 11:01:42 -0500

    Initial Commit

---
+++ templateProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  templateProject
+//
+//  Created by Thibault Gauche on 12/9/13.
+//  Copyright (c) 2013 fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
